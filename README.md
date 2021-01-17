ddfe8e07a04f06dabf811c435e0ae5b05d2f715c
Francesco Urbani <francescourbanidue@gmail.com>
2021-01-17 23:11:48 +0100

    added histogram plot and everything needed for a first bare version.

--- README.md
+++ README.md
@@ -12,3 +12,62 @@ cd ~/Downloads/Telegram\ Desktop/ChatExport_2021-01-15
 # run telegrust-histo 
 telegrust-histo 
 ```
+
+
+![](https://raw.githubusercontent.com/urbanij/telegrust-histo/master/histogram.svg)
+
+#### Demo:
+
+```sh
+~/D/T/ChatExport_2021-01> time ~/telegrust-histo/target/release/telegrust-histo
+Renaming messages.html to messages1.html
+Processing messages1.html
+Processing messages2.html
+Processing messages3.html
+Processing messages4.html
+Processing messages5.html
+Processing messages6.html
+Processing messages7.html
+Processing messages8.html
+Processing messages9.html
+Processing messages10.html
+Processing messages11.html
+Processing messages12.html
+Processing messages13.html
+Processing messages14.html
+Processing messages15.html
+Processing messages16.html
+Processing messages17.html
+Processing messages18.html
+Processing messages19.html
+Processing messages20.html
+Processing messages21.html
+Processing messages22.html
+Processing messages23.html
+Processing messages24.html
+Processing messages25.html
+Processing messages26.html
+Processing messages27.html
+Processing messages28.html
+Processing messages29.html
+Processing messages30.html
+Processing messages31.html
+Processing messages32.html
+Processing messages33.html
+Processing messages34.html
+Processing messages35.html
+Processing messages36.html
+Processing messages37.html
+Processing messages38.html
+Processing messages39.html
+Processing messages40.html
+Processing messages41.html
+Processing messages42.html
+Processing messages43.html
+Processing messages44.html
+Processing messages45.html
+Processing messages46.html
+Processing messages47.html
+Renaming messages1.html to messages.html
+  0.23s user 0.06s system 70% cpu 0.414 total
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,79 @@
+/**
+ * author(s):   @urbanij
+ * date:        Sun 17 Jan 2021 23:06:12 CET
+ *
+ * description: Generate a messages-density histogram of your exported Telegram chat.
+ *              └── why? I was curious, and Python was too slow(*) at parsing timestamps and so on.
+ *
+ *                    (*) 0.23 seconds vs 45 seconds benchmarked on the same ~45k messages dataset.
+ */
+
+ 
+use std::fs; // renaming
 use std::fs::File;
 use std::io::prelude::*;
-use std::fs::OpenOptions; // opening file
+// use std::fs::OpenOptions; // opening file
 
-use soup::prelude::*;
-
-use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime};
+use chrono::NaiveDateTime;
+// use chrono::{DateTime, NaiveDate, NaiveTime};
 use chrono::format::ParseError;
 
+use regex::Regex;
 
+use plotlib::page::Page;
+use plotlib::repr::{Histogram, HistogramBins};
+use plotlib::style::BoxStyle;
+use plotlib::view::ContinuousView;
+
+const NUM_BINS: usize = 200;
+
+
+fn main() {
+        
+    let REGEX_DATE_PATTERN = Regex::new(r"(\d{2}.\d{2}.\d{4} \d{2}:\d{2}:\d{2})").unwrap();
+    
+    let mut timestamps = vec![];
+
+    rename_file("messages.html", "messages1.html").unwrap();
+
+    let mut i = 1;
+    while let Ok(content) = read_file( format!("messages{}.html", i).as_str() ) {
+        println!("Processing {}", format!("messages{}.html", i));
+        i += 1;
+
+        for caps in REGEX_DATE_PATTERN.captures_iter(content.as_str()) {
+            
+            let epoch_date = tg_date_to_epoch_date(caps.get(1).unwrap().as_str());
+            match epoch_date {
+                Ok(n) => {
+                    // println!("{:?}, ", n);
+                    timestamps.push(n as f64);
+                },
+                Err(e) => println!("Error: {}", e),
+            }
+
+        }
+    }
+
+    generate_plot(&timestamps);
+
+    rename_file("messages1.html", "messages.html").unwrap();
+}
+
+fn generate_plot(data: &Vec<f64>) {
+    let h = Histogram::from_slice(&data, HistogramBins::Count(NUM_BINS))
+        .style(&BoxStyle::new().fill("burlywood"));
+
+    let v = ContinuousView::new().add(h);
+
+    Page::single(&v).save("histogram.svg").expect("saving svg");
+}
+
+fn rename_file(original: &str, new_name: &str) -> std::io::Result<()> {
+    fs::rename(original, new_name)?;
+    println!("Renaming {} to {}", original, new_name);
+    Ok(())
+}
 
 fn read_file(filename: &str) -> std::io::Result<String> {
     let mut file = File::open(filename)?;
@@ -17,26 +83,8 @@ fn read_file(filename: &str) -> std::io::Result<String> {
     Ok(content)
 }
 
-fn main() {
-
-    let content = read_file("messages.html").expect("File not found");
-
-    let soup = Soup::new(content.as_str());
-
-    let p = soup.tag("div").find().expect("Couldn't find tag 'p'");
-    
-    println!("{:?}", p.get("class"));
-
-    let a = parse_stuff();
-    print!("{:?}", a);
-
+fn tg_date_to_epoch_date(tg_date: &str) -> Result<i64, ParseError> {
+    let ts = NaiveDateTime::parse_from_str(tg_date, "%d.%m.%Y %H:%M:%S")?;
+    Ok(ts.timestamp())
 }
 
-
-fn parse_stuff() -> Result<(), ParseError> {
-
-    let ts = NaiveDateTime::parse_from_str("26.11.2019 21:55:36", "%d.%m.%Y %H:%M:%S")?;
-    println!("{:?}", ts.timestamp());
-
-    Ok(())
-}
