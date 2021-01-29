01ece45e6690d2cca5550d9ac989ee9b0baa0e19
Francesco Urbani <francescourbanidue@gmail.com>
2021-01-29 21:41:57 +0100

    minor update - improved error messages and some refactoring. - removed that awful file renaming

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Why? I was curious, and [Python proved too slow](https://github.com/urbanij/telegram-histo)<sup>1</sup> at parsing timestamps, although the plot looks nicer there.
 
-<sup>1</sup> 0.23 seconds vs 30 seconds benchmarked on the same ~45k messages dataset.
+<sup>1</sup> 0.3 seconds vs 30 seconds benchmarked on the same ~45k messages dataset.
 
 
 #### Usage:
@@ -12,7 +12,7 @@ Why? I was curious, and [Python proved too slow](https://github.com/urbanij/tele
 # provided that you have the rust toolchain set up.
 cargo install telegrust-histo
 
-# cd to wherever your exported folder is, in my case:
+# cd to wherever your exported folder is
 cd ~/Downloads/Telegram\ Desktop/ChatExport_2021-01-15 
 
 # see telegrust-histo options
@@ -23,21 +23,13 @@ telegrust-histo -b 300 -v
 ```
 
 
-![](https://github.com/urbanij/telegrust-histo/blob/main/histogram.svg?raw=true)
+![](https://github.com/urbanij/telegrust-histo/blob/main/histogram_200.svg?raw=true)
 
 #### Demo:
 
 ```sh
-~/ChatExport_2021-01-15> telegrust-histo -b 100 -v
-Renaming messages.html to messages1.html
-Processing messages1.html
-Processing messages2.html
-Processing messages3.html
-Processing messages4.html
-Processing messages5.html
-Processing messages6.html
-Processing messages7.html
-Processing messages8.html
-Processing messages9.html
-Renaming messages1.html to messages.html
+~/ChatExport_2021-01-15> time telegrust-histo -b 200  # time is optional btw of course
+[+] Generated histogram_200.svg (using 200 bins) in the current folder.
+[+] Processed 47 files (46610 messages) from 2017-04-16 22:02 to 2021-01-13 13:30
+telegrust-histo  0.27s user 0.02s system 97% cpu 0.295 total
 ```
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
  */
 
  
-use std::fs; // renaming
+// use std::fs; // renaming
 use std::fs::File;
 use std::io::prelude::*;
 // use std::fs::OpenOptions; // opening file
@@ -22,16 +22,15 @@ use plotlib::view::ContinuousView;
 
 use argparse::{ArgumentParser, StoreTrue, Store};
 
-use std::process;
-
 use chrono::prelude::DateTime;
 use chrono::Utc;
 use std::time::{SystemTime, UNIX_EPOCH, Duration};
 
-use termion::clear;
 
-
-fn generate_plot(data: &Vec<f64>, num_bins: usize, verbose: bool) {
+fn generate_plot(
+    data: &Vec<f64>, 
+    num_bins: usize) 
+{
     let h = Histogram::from_slice(
                             &data, 
                             HistogramBins::Count(num_bins))
@@ -45,28 +44,28 @@ fn generate_plot(data: &Vec<f64>, num_bins: usize, verbose: bool) {
 
     let v = ContinuousView::new()
         .add(h)
-        .x_label("date")
+        .x_label("date (epoch time)")
         .y_label("count");
 
-    const FILE_NAME: &str = "histogram.svg";
-    Page::single(&v).save(FILE_NAME).expect("saving svg");
+    let file_name = format!("histogram_{}.svg", num_bins);
 
-    println!("Generated {} (using {} bins) in the current folder.", FILE_NAME, num_bins);
+    Page::single(&v).save(&file_name).expect("saving svg");
+
+    println!("[+] Generated {} (using {} bins) in the current folder.", file_name, num_bins);
 }
 
-fn rename_file(original: &str, new_name: &str, verbose: bool) -> std::io::Result<()> {
-    fs::rename(original, new_name)?;
-    if verbose {
-        println!("Renamed {} to {}", original, new_name);
-    }
-    Ok(())
-}
+// fn rename_file(original: &str, new_name: &str, verbose: bool) -> std::io::Result<()> {
+//     fs::rename(original, new_name)?;
+//     if verbose {
+//         println!("Renamed {} to {}", original, new_name);
+//     }
+//     Ok(())
+// }
 
 fn read_file(filename: &str) -> std::io::Result<String> {
     let mut file = File::open(filename)?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
-    // println!("{}", content);
     Ok(content)
 }
 
@@ -81,70 +80,88 @@ fn epoch_to_readable_date(epoch_date: f64) -> String {
     // Create DateTime from SystemTime
     let datetime = DateTime::<Utc>::from(d);
     // Formats the combined date and time with the specified format string.
-    let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
+    let timestamp_str = datetime.format("%Y-%m-%d %H:%M").to_string();
 
     timestamp_str
 }
 
+fn process_content(content: String, timestamps: &mut Vec<f64>, file_num: Option<i32>, verbose: bool) {
+    
+    let regex_date_pattern = Regex::new(r"(\d{2}.\d{2}.\d{4} \d{2}:\d{2}:\d{2})").unwrap();
+                                    // matches timestamps of this kind: 13.12.2018 19:17:39
+    
+    if verbose {
+        match file_num {
+            Some(v) => println!("Processing messages{}.html", v),
+            None => println!("Processing messages.html"),
+        };
+    }
+
+    for caps in regex_date_pattern.captures_iter(content.as_str()) {
+        let epoch_date = tg_date_to_epoch_date(caps.get(1).unwrap().as_str());
+        match epoch_date {
+            Ok(n) => timestamps.push(n as f64),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+}
+
 fn main() {
+
+    // default values of argparsed arguments
     let mut verbose = false;
     let mut num_bins = 200;
 
     {  // this block limits scope of borrows by ap.refer() method
         let mut ap = ArgumentParser::new();
         ap.set_description("[telegrust-histo](https://github.com/urbanij/telegrust-histo)");
-        ap.refer(&mut verbose)
-            .add_option(&["-v", "--verbose"], 
-                        StoreTrue,
-                        "Be verbose");
         ap.refer(&mut num_bins)
             .add_option(&["-b"], 
                         Store,
                         "num_bins in your histogram");
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], 
+                        StoreTrue,
+                        "Be verbose");
         ap.parse_args_or_exit();
     }
         
-    let regex_date_pattern = Regex::new(r"(\d{2}.\d{2}.\d{4} \d{2}:\d{2}:\d{2})").unwrap();
-                                    // matches timestamps of this kind: 13.12.2018 19:17:39
+    
     
     let mut timestamps = vec![];
 
-    rename_file("messages.html", "messages1.html", verbose).unwrap();
+    // first file has no number in it, the following have an incremental suffix 
+    // before the file extension, thus i iterate those in a loop.
 
-
-    let mut i = 1;
-    while let Ok(content) = read_file( format!("messages{}.html", i).as_str() ) {
-        if verbose {
-            println!("Processing messages{}.html", i);
-            // clear::CurrentLine();
-        }
-
-        for caps in regex_date_pattern.captures_iter(content.as_str()) {
-            let epoch_date = tg_date_to_epoch_date(caps.get(1).unwrap().as_str());
-            match epoch_date {
-                Ok(n) => timestamps.push(n as f64),
-                Err(e) => println!("Error: {}", e),
-            }
-        }
-        i += 1;
-
+    match read_file("messages.html") {
+        Ok(content) => process_content(content, &mut timestamps, None, verbose),
+        Err(e) => {
+            println!("{:?}", e);
+            println!("[-] `messages.html` not found!");
+            println!("    Make sure you're running `telegrust-histo` inside the root directory of your exported chat.");
+            std::process::exit(exitcode::DATAERR);
+        },
+    }
+    
+    let mut num_files = 2;
+    while let Ok(content) = read_file( format!("messages{}.html", num_files).as_str() ) {
+        process_content(content, &mut timestamps, Some(num_files), verbose);
+        num_files += 1;
     }
 
     // println!("{:?}", timestamps);
     if timestamps.len() <= 0 {
-        eprintln!("Some errors with your files... Quitting.");
+        eprintln!("[-] Some errors with your files... Quitting.");
         std::process::exit(exitcode::DATAERR);
     }
 
-    generate_plot(&timestamps, num_bins, verbose);
+    generate_plot(&timestamps, num_bins);
     
-    println!("Processed {} messages from {} to {}", 
+    println!("[+] Processed {} files ({} messages) from {} to {}", 
+        num_files - 1,
         timestamps.len(),
         epoch_to_readable_date(timestamps[0]),
         epoch_to_readable_date(timestamps[timestamps.len()-1]),
     );
 
-    rename_file("messages1.html", "messages.html", verbose).unwrap();
 }
-
-
